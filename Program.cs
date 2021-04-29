60baf8aa43737160d8f86645f811e0f576eebfa7
João Silva <joaoandre.m.silva@gmail.com>
2021-04-29 01:28:57 +0100

    Project restructuring and update from .Net Core 3.1 to .Net 5.0.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FNAGame
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			// https://github.com/FNA-XNA/FNA/wiki/7:-FNA-Environment-Variables#fna_graphics_enable_highdpi
+			// NOTE: from documentation:
+			//       Lastly, when packaging for macOS, be sure this is in your app bundle's Info.plist:
+			//           <key>NSHighResolutionCapable</key>
+			//           <string>True</string>
+			Environment.SetEnvironmentVariable("FNA_GRAPHICS_ENABLE_HIGHDPI", "1");
+
+			using var game = new FNAGame();
+			game.Run();
+		}
+	}
+}
